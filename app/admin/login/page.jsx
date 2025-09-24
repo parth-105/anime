6983423b8bed881b30214e0caecd28e05862f728
parth-105 <harshkalathiya7@gmail.com>
2025-09-24 19:33:58 +0530

    add some changes

--- app/admin/login/page.jsx
+++ app/admin/login/page.jsx
@@ -5,8 +5,8 @@ import { useRouter } from 'next/navigation'
 
 export default function AdminLogin(){
   const router = useRouter()
-  const [email, setEmail] = useState('admin@admin.com')
-  const [password, setPassword] = useState('admin@admin.com')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const [err, setErr] = useState('')
   const [loading, setLoading] = useState(false)
 
